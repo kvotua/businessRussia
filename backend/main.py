80542973cf90bca1100fc950c6e9558b11775d64
Ekaterina <kvotua.job@gmail.com>
2024-12-21 12:46:27 +0200

    ref for deploy

--- backend/main.py
+++ backend/main.py
@@ -6,15 +6,14 @@ from models.database_model import Base
 
 #Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
-origins = "https://business-russia.kvotua.ru"
+debug = os.getenv("DEBUG") is not None
+app = FastAPI(debug=debug)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins], 
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"], 
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
 
 app.include_router(router)
