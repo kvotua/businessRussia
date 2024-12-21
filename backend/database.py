b3c55e939421a2950370b9b7e1fb92b358460d4b
Ekaterina <kvotua.job@gmail.com>
2024-12-21 13:30:16 +0200

    ref for deploy

--- backend/database.py
+++ backend/database.py
@@ -11,3 +11,8 @@ engine = create_engine(DB_URL,echo=True)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
 
 Base.metadata.create_all(bind=engine)
+
+with engine.connect() as connection:
+    connection.execute(text("ALTER DATABASE fastapi_db CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;"))
+    connection.execute(text("ALTER TABLE users_notify CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"))
+    connection.execute(text("ALTER TABLE business_requests CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"))
